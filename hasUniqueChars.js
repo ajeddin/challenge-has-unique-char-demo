1194b465f1a8ae28e38878072e6377b1d6169b2a
ajeddin <abduljeddin@gmail.com>
2022-11-09 20:27:58 +0300

    Done

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,1 +1,23 @@
 // Write your code below
+word = 'hgellooo'
+falesCheck = true
+function hasUniqueChars(word){
+    wordNew = word[0]  
+    let wordArray= [];
+
+    for(i=0;i < word.length;i++){
+    if (wordArray.includes(word[i])){
+        falesCheck =  false
+        
+    }else{
+        wordArray.push(word[i])
+        wordNew= word[i]
+    }
+}if (falesCheck=== false){
+    console.log('false');
+}else{
+console.log('true');
+
+}}
+console.log('----------');
+hasUniqueChars(word)
